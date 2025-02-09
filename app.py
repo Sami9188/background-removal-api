0f513499513e2bbc04ad7a2486305aab9d8a2574
Muhammad Sami Akran <96423739+Sami9188@users.noreply.github.com>
2025-02-09 20:10:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from rembg import remove
 from PIL import Image
 import io
 
 app = Flask(__name__)
+CORS(app)  # Allow all CORS requests
 
 @app.route('/')
 def index():
