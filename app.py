5989d2d7c828f63e16328437342a099959f8596a
Muhammad Sami Akran <96423739+Sami9188@users.noreply.github.com>
2025-02-09 17:13:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def remove_bg():
 if __name__ == '__main__':
     # Use Waitress for production deployment.
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5001)
+    serve(app, host='0.0.0.0', port=5000)
